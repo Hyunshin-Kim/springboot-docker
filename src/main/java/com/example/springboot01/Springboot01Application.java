8704277f20df1b98258a1243a645b9f274ad0e84
mg0309v <mg0309v@gmail.com>
2024-05-17 10:29:36 +0900

    test

--- src/main/java/com/example/springboot01/Springboot01Application.java
+++ src/main/java/com/example/springboot01/Springboot01Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Springboot01Application {
-
+    // Main
     public static void main(String[] args) {
         SpringApplication.run(Springboot01Application.class, args);
     }
